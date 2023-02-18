a6f99aead8133a1d9989aa81f8f8e635a936f34a
Назар <105419931+Naza1re@users.noreply.github.com>
2023-02-18 15:13:53 +0300

    Add files via upload

---
+++ lab 1.2/lab 1.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace std;
+
+class Program
+{
+
+    public static void Main(String[] args)
+    {
+        double x;
+        double y;
+     
+        do
+        {
+            Console.WriteLine("Есть условие которое надо выполнить, кароче число А длолжно быть больше Б");
+            Console.WriteLine("Введите число А");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введиет число Б");
+            y = Convert.ToInt32(Console.ReadLine());
+           
+
+        } while (!(x>= y));
+        double g = x / y;
+        Console.WriteLine("Поместится " + g + " Раз");
+
+    }
+
+  
+
+}
