1651691987784a4c29c9e98a5a06733d83699d4d
Назар <105419931+Naza1re@users.noreply.github.com>
2023-02-18 15:07:23 +0300

    Add files via upload

---
+++ lab 1.1/lab 1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace std;
+
+class Program
+{
+
+    public static void Main(String[] args)
+    {
+        double a;
+
+
+        Console.WriteLine("Введите сторону квадрата");
+
+        a = Convert.ToInt32(Console.ReadLine());
+
+        double b = a * a;
+
+        Console.WriteLine("Площадь квадрата равна : " + b);
+
+        double c = a * 4;
+
+        Console.WriteLine("Периметр квадрата равен :" + c);
+    }
+}
+       
+
+
