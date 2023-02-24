c0f4159877c4434b4be2ad04388cb97fbcce0fef
Назар <105419931+Naza1re@users.noreply.github.com>
2023-02-24 22:57:11 +0300

    Add files via upload

---
+++ lab 2.1/lab 2.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace std;
+
+class Program
+{
+
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Введите число для определния его знака");
+
+
+        double a = Convert.ToInt32(Console.ReadLine());
+        bool b = false;
+
+        if (a > 0)
+        {
+            b = true;
+            Console.WriteLine(b+ "Данное число больше нуля");
+        }
+        else
+        {
+            Console.WriteLine(b+ "Даное чилсо меньше нуля");
+
+
+        }
+
+
+
+    }
+}
---
+++ lab 2.2/lab 2.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace std;
+class Program
+{
+
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Введите число a");
+        double a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число б");
+        double b = Convert.ToInt32(Console.ReadLine());
+
+
+
+        if (a > b)
+        {
+            Console.WriteLine("Число а больше чилса б");
+
+        }
+        else
+            Console.WriteLine("Число б больше чилса а");
+
+
+    }
+
+
+}
